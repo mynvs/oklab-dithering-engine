3af21bb73624bf503c3d078fd64b8b438222d3ea
Architect Open Source Department <59548080+architect-ent-opnsrc@users.noreply.github.com>
2023-05-06 07:33:05 -0700

    &&

--- make-palette.cmd
+++ make-palette.cmd
@@ -1,1 +1,1 @@
-mkdir bin cd src && g++ -pedantic -Wall -ffast-math -I../include make-palette.cpp -o ../bin/make-palette -O3 && cd .. && bin/make-palette
+mkdir && bin cd src && g++ -pedantic -Wall -ffast-math -I../include make-palette.cpp -o ../bin/make-palette -O3 && cd .. && bin/make-palette
