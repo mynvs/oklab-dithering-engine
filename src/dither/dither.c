12015cb4ab5b8ba83eb76c932da2ca97b6eb8dee
architectnt <eno@i2iproductions.com>
2025-09-20 22:45:53 -0700

    include string

--- src/dither/dither.c
+++ src/dither/dither.c
@@ -1,5 +1,6 @@
 #include <limits.h>
 #include <float.h>
+#include <string.h>
 #include "dither.h"
 #include "canvas.h"
 #include "../utils/fileutils.h"
--- src/utils/fileutils.h
+++ src/utils/fileutils.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #ifdef _WIN32
 #include <windows.h>
 #include <direct.h>
